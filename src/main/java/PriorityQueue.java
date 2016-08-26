a8ee5fd6e885f9dd607e5f3bf64cf130ef0d03d4
Ashwin Rao <theonlyrao@gmail.com>
2016-08-26 18:15:27 -0500

    Implement ability to get max priority

--- src/main/java/PriorityQueue.java
+++ src/main/java/PriorityQueue.java
@@ -24,7 +24,29 @@ public class PriorityQueue implements PriorityQueueI {
     }
 
     public int getMax() {
+	int max = queue[1];
+	int maxChild;
+	queue[1] = queue[numQueueItems--];
 
+	int i;
+	int child;
+	for ( int i = 1; i * 2 <= numQueueItems; i = i * 2 ) {
+	    if ( queue[i * 2] > queue[(i * 2) + 1]) {
+		child = i * 2;
+	    } else {
+		child = (i * 2) + 1;
+	    }
+	    maxChild = queue[child];
+	    
+	    if (tmp > queue[i]) {
+		queue[child] = queue[i];
+		queue[i] = maxChild;
+	    } else {
+		break;
+	    }
+	}
+
+	return max;
+	
     }
-
 }
